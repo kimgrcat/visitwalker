c783a181ae44154369f44929ad86f0074d1f79b0
Kim Granberg <joacim.granberg@bisnode.com>
2023-04-18 13:29:36 +0200

    init add

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const TodoListRoutes = require('./routes/api/Todolist')
+const path = require('path')
+const dotenv = require('dotenv');
+dotenv.config({ path: path.resolve(__dirname, './develop.env') })
+
+app.use(cors())
+app.use(bodyParser.json())
+
+mongoose
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+
+    })
+    .then(() => console.log('MongoDB database Connected...'))
+    .catch((err) => console.log(err))
+
+app.use('/api/todoList', TodoListRoutes)
+
+app.listen(3000, () => console.log(`App listening at http://localhost:3000`))
